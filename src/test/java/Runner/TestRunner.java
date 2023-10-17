abd54d298f03134b4c51132fff57cfdf4f22e54e
JibyRaju <81110280+JibyRaju@users.noreply.github.com>
2023-10-17 13:44:12 -0400

    Fourth Commit

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 									"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-									"html:Reports/Cucumber-Reports/Reports.json"},
+									"html:Reports/Cucumber-Reports/Reports.html",
+									"json:Reports/Cucumber-reports/Reports.json"},
 
 monochrome = false,
 features = { "src/test/resources/features" }, 
